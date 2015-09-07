75128200e0625e4578de3cd178eef28c31c24e05
vsooda <liushouda@qq.com>
2015-09-07 17:13:37 +0800

    word index

--- test/cutindex_test.py
+++ test/cutindex_test.py
@@ -7,14 +7,20 @@ import os
 from util.cut2index import *
 from config import cfg
 
+def pickle_test():
+    wc = pickle.load(open('wordcount.pkl', 'rb'))
+    for k, v in wc.items():
+        print k , v
+
+
 if __name__ == "__main__":
+    #pickle_test()
     stcpath = os.path.join(cfg.ROOT_DIR, cfg.DATAPATH)
     postFile = os.path.join(stcpath, cfg.POST_FILENAME)
     commentFile = os.path.join(stcpath, cfg.COMMENT_FILENAME)
     postFileFiltered = postFile + cfg.FILTER_POSTFIX
     commentFileFiltered = commentFile + cfg.FILTER_POSTFIX
     print postFileFiltered
-
     cut2index(postFileFiltered, commentFileFiltered)
 
 
