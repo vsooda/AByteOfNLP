dda6ddf82e50a57be6f1c1e67a68c9d68879fde3
vsooda <liushouda@qq.com>
2016-04-25 18:28:18 +0800

    filter pinyin word

--- util/tts_pinyin.py
+++ util/tts_pinyin.py
@@ -67,8 +67,6 @@ def dump_pinyin_dict(dict_name):
         fout.write('\n')
 
 
-
-
 def change_han_pinyin_dir(from_dir, to_dir, punt_dict):
     filenames = [f for f in listdir(from_dir) if isfile(join(from_dir, f)) ]
     for f in filenames:
@@ -136,18 +134,82 @@ def change_han_pinyin_test():
     change_han_pinyin_dir(tts_text, save_path, punt_dict)
 
 def dump_pinyin_dict_test():
+    root_dir = cfg.ROOT_DIR
     pinyinDict = os.path.join(root_dir, 'data/pinyin.txt')
     dump_pinyin_dict(pinyinDict)
 
 def change_pinyin_dict_test():
+    root_dir = cfg.ROOT_DIR
     pinyinDict = os.path.join(root_dir, 'data/pinyin.txt')
     change_dict_format(pinyinDict)
+
+#filter out not use pinyin_word: not in pinyin_dict; all right;
+def mix_pinyin_word_dict(pinyin_path, pinyin_word_path, save_name):
+    pinyin_dict = {}
+    with open(pinyin_path) as f:
+        for line in f:
+            k, v = line.decode('utf-8').strip().split('\t')
+            pinyins = v.lower().split(" ")
+            #if (len(pinyins) > 1) :
+            han = unichr(int(k, 16))
+            pinyin_dict[han] = pinyins
+            #print  han, " ".join(pinyins)
+    f.close()
+    #for han, pinyins in pinyin_dict.items():
+    #    print han, pinyins
+    mix_lines = []
+    with open(pinyin_word_path) as f:
+        for line in f:
+            line = line.decode('utf-8').strip()
+            k, v = line.split(':')
+            k = k.strip()
+            v = v.strip()
+            save_flag = True
+            all_default = True
+            pinyinss = ''
+            for i in range(len(k)):
+                han = k[i]
+                line_pinyins = v.split(" ")
+                pinyin = pinyin_dict.get(han, None)
+                if pinyin is not None:
+                    pinyinss = pinyinss + pinyin[0]
+                    if pinyin[0] != line_pinyins[i]: #is the same with the default pinyin
+                        all_default = False
+                        if line_pinyins[i] not in pinyin:
+                            print "not in list:break it", han, line_pinyins[i], 'in pinyin list: ', line, ' '.join(x for x in pinyin)
+                            save_flag = False # not in pinyin list
+                            break
+                else:
+                    print "can not find pinyin for ", han, pinyin
+                    save_flag = False
+                    break
+            if save_flag and not all_default:
+                print 'saving', line, pinyinss
+                mix_lines.append(line)
+            else:
+                print 'no saving', line, pinyinss
+    f.close()
+
+    with open(save_name, 'w') as f:
+        for line in  mix_lines:
+            f.write(line)
+            f.write('\n')
+    f.close()
+
+# read the pinyin dict and pinyin_word;remove the useless pinyin_word
+def mix_pinyin_word_dict_test():
+    root_dir = cfg.ROOT_DIR
+    pinyin_path = os.path.join(root_dir, 'data/pinyin.txt')
+    pinyin_word_path = os.path.join(root_dir, 'data/pinyin_word.txt')
+    save_name = "mix_pinyin_word.txt"
+    mix_pinyin_word_dict(pinyin_path, pinyin_word_path, save_name)
+
 
 if __name__ == '__main__':
     #use glob can list files very easy. but need to cut the path
     #print glob.glob("/home/sooda/data/tts_data/text/*.txt")
-    root_dir = cfg.ROOT_DIR
     #test_punt_dict(punt_path)
     #change_han_pinyin_test()
-    change_pinyin_dict_test()
+    #change_pinyin_dict_test()
+    mix_pinyin_word_dict_test()
 
